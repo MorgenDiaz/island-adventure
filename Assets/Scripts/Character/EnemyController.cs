9eb51cec382c7894530aa3d1b1928c672c163789
Morgen Diaz <morgen.diaz@fullstackacademy.com>
2024-07-15 21:17:12 -1000

    Create base enemy prefab. Create stationary enemy guard variant off of base enemy. Configure enemy interaction with navigation mesh.

---
+++ Assets/Scripts/Character/EnemyController.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace RPG.Character {
+    public class EnemyController : Monobehavior {
+        public float chaseRange = 2.5f;
+        public float attackRange = 0.75f;
+    }
+}
