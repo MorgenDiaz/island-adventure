304249b55402570c23936a004067192b4a32eb6c
Morgen Diaz <morgen.diaz@fullstackacademy.com>
2024-07-15 17:33:57 -1000

    Configure walkable terrain using nav mesh. Implement player movement. Configure initial camera to follow player using cinemachine.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.AI;
+
+namespace RPG.Character {
+    public class Movement : MonoBehaviour {
+        private Vector3 movementVector;
+        public NavMeshAgent agent;
+
+        protected void Awake() {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        protected void Update() {
+            MovePlayer();
+        }
+
+        private void MovePlayer() {
+            agent.Move(movementVector);
+        }
+        public void HandleMove(InputAction.CallbackContext context) {
+            Vector2 input = context.ReadValue<Vector2>();
+            movementVector = new Vector3(input.x, 0, input.y);
+
+            print("WASD clicked! " + movementVector);
+        }
+    }
+}
