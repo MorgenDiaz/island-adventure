599fcb2a7f754d6e88af6a735fce988ffc9b03a4
Morgen Diaz <morgen.diaz@fullstackacademy.com>
2024-07-24 21:41:39 -1000

    Create initial Dialogue UI.

--- Assets/Scripts/Character/FriendlyNPC.cs
+++ Assets/Scripts/Character/FriendlyNPC.cs
@@ -1,13 +1,18 @@
 using RPG.Utility;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace RPG.Character {
     public class FriendlyNPC : MonoBehaviour {
         [SerializeField]
-
         private GameObject _interactionIndicator;
         [SerializeField]
         private GameObject _dialogIndicator;
+
+        [SerializeField]
+        TextAsset InkDialogue;
+
+        private bool _isPlayerInInteractionRange = false;
 
         private void Awake() {
             _dialogIndicator.SetActive(false);
@@ -17,14 +22,22 @@ namespace RPG.Character {
             if (other.CompareTag(Constants.Tags.PLAYER)) {
                 _interactionIndicator.SetActive(false);
                 _dialogIndicator.SetActive(true);
+                _isPlayerInInteractionRange = true;
             }
         }
         private void OnTriggerExit(Collider other) {
             if (other.CompareTag(Constants.Tags.PLAYER)) {
                 _dialogIndicator.SetActive(false);
                 _interactionIndicator.SetActive(true);
+                _isPlayerInInteractionRange = false;
             }
         }
 
+        public void HandleInteraction(InputAction.CallbackContext context) {
+            if (!context.performed || !_isPlayerInInteractionRange) return;
+            if (InkDialogue == null) return;
+
+            print("Interact Yo!");
+        }
     }
 }
